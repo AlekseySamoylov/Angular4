17bdf3a6ab4afc1b56e768a0e53a0f0e436909c3
AlekseySamoylov <alekseysamoylov89@gmail.com>
2017-06-03 11:39:45 +0300

    ngIf ngForOf

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
-  variable = 'Hello';
+  serverId: number;
+  serverStatus: string;
+  isServerOnline = false;
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  constructor() {
+    const randomValue: number = Math.random();
+    this.isServerOnline = randomValue > 0.5;
+    this.serverStatus = this.isServerOnline ? 'online' : 'offline';
+    this.serverId = this.isServerOnline ? Math.trunc(randomValue * 10) : null;
+  }
+
+  getColor(): string {
+    return this.isServerOnline ? 'green' : 'red';
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+
+
+class FileServer {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server created';
+  serverCreated: boolean;
+  serverName: string;
+  serverList: Array<FileServer> = [{id: 1, name: 'Microsoft'}, {id: 2, name: 'Oracle'}];
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Now Server ' + this.serverName + ' was created!';
+    this.serverCreated = true;
+    const serverListLength: number = this.serverList.length;
+    let lastServerId: number = this.serverList[serverListLength - 1].id;
+    lastServerId++;
+    // this.serverList[serverListLength] = {id: lastServerId, name: this.serverName};
+    this.serverList.push({id: lastServerId, name: this.serverName});
+    this.serverName = null;
+  }
+
+  // onUpdateServerName(event: Event) {
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+  //   console.log(event);
+  // }
 }
